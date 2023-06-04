90213306881a01476dc549feed66259cd1ea6908
Earl Bit <earlbit247@gmail.com>
2023-06-04 23:27:31 +0200

    JS13

---
+++ main.js
@@ -0,0 +1,11 @@
+function checkNumberRange(n) {
+    if (n >= 100 && n <= 200) {
+      console.log("The number is in the range");
+    } else {
+      console.log("Number is not in range");
+    }
+  }
+  
+  // Example usage: calling the function with a number
+  checkNumberRange(150);
+  
